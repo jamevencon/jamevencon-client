03bb28c4015650b14a95fd845d724fd904e53c7a
YEAHx4 <developerstarlight@gmail.com>
2022-12-08 13:33:19 +0900

    star background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,46 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage<{}> = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Head>
+        <title>JamEvenCon</title>
+      </Head>
+
+      <div className="bg">
+        <div className="bg-front"></div>
+        <div className="bg-back"></div>
+      </div>
+
+      <style jsx>{`
+        .bg {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          z-index: -1;
+        }
+
+        .bg > div {
+          position: absolute;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-size: 1500px 750px;
+        }
+
+        .bg-front {
+          background-image: url("/background-star-back.png");
+        }
+
+        .bg-back {
+          background-image: url("/background-star-front.png");
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Home;
