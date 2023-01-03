f43ebb850637bacb63cf0674cb9d29223108c798
YEAHx4 <developerstarlight@gmail.com>
2023-01-03 23:43:42 +0900

    better input - Multiple line input with Shift + Enter

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { useEffect } from "react";
 
 interface Props {
   input: string;
@@ -16,48 +17,62 @@ const Input: NextPage<Props> = ({
   useFormer,
   useLatter,
   newLine,
-}) => (
-  <>
-    <textarea
-      placeholder="Start your journey"
-      value={input}
-      onChange={(e) => {
-        setInput(e.target.value);
-      }}
-      onKeyDown={(e) => {
-        // This will allow for non-english word
-        // (especially composition letters) to be typed properly
-        if (e.nativeEvent.isComposing) return;
+}) => {
+  // In dev server, change state executes twice.
+  // So, \n will be appended twice.
+  useEffect(() => {
+    if (input.endsWith("\n\n")) {
+      setInput(input.trimEnd() + "\n");
+    }
+  }, [input]);
 
-        if (e.key === "Enter") {
-          if (input === "") {
-            newLine();
-            return;
+  return (
+    <>
+      <textarea
+        placeholder="Start your journey"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          // This will allow for non-english word
+          // (especially composition letters) to be typed properly
+          if (e.nativeEvent.isComposing) return;
+
+          if (e.key === "Enter") {
+            if (e.shiftKey) {
+              setInput(input + "\n");
+              return;
+            }
+            if (input === "") {
+              newLine();
+              return;
+            }
+
+            execute(input.trim());
+            setInput("");
+          } else if (e.key === "ArrowUp") useFormer();
+          else if (e.key === "ArrowDown") useLatter();
+        }}
+      ></textarea>
+
+      <style jsx>
+        {`
+          textarea {
+            background-color: rgb(10, 10, 27, 0.6);
+            font-family: D2Coding, Consolas;
+            height: 15%;
+            color: white;
+            font-size: 1.5rem;
+            overflow-x: scroll;
+            word-break: break-all;
+            border: none;
+            outline: none;
           }
-
-          execute(input.trim());
-          setInput("");
-        } else if (e.key === "ArrowUp") useFormer();
-        else if (e.key === "ArrowDown") useLatter();
-      }}
-    ></textarea>
-
-    <style jsx>
-      {`
-        textarea {
-          background-color: rgb(10, 10, 27, 0.6);
-          font-family: D2Coding, Consolas;
-          height: 15%;
-          color: white;
-          font-size: 1.5rem;
-          overflow-x: scroll;
-          word-break: break-all;
-          border: none;
-          outline: none;
-        }
-      `}
-    </style>
-  </>
-);
+        `}
+      </style>
+    </>
+  );
+};
 
 export default Input;
